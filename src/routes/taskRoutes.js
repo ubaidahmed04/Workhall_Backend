faa42703162ffac194dac62a50b92f3e4d2f308a
ubaidahmed04 <obaidkhanzada2004@gmail.com>
2026-06-23 18:05:12 +0500

    feat: Refactor API routes to improve structure and consistency

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const { addEditTask } = require("../controllers/taskController");
 
-router.post("/task/app/add-edit", addEditTask);
+router.post("/task/add-edit", addEditTask);
 
 module.exports = router;
